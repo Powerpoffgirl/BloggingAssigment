e19b5fd141fda44cfa3ff7bf508dceab6e15d580
Jyoti Singh <email.jyotisinsh13@gmail.com>
2023-07-30 09:52:18 +0530

    c

--- Controllers/AuthController.js
+++ Controllers/AuthController.js
@@ -118,21 +118,23 @@ AuthRouter.post("/logout", isAuth, async(req, res) => {
   console.log("REQUEST SESSION BEFORE LOGOUT", req.session)
   const user = req.session.user;
 
- await req.session.destroy((err) => {
-    if (err) {
-      return res.send({
-        status: 400,
-        message: "Logout unsuccessfull",
-        error: err,
-      });
-    }
-
+  req.session.destroy()
+  
+  // (err) => {
+    // if (err) {
+    //   return res.send({
+    //     status: 400,
+    //     message: "Logout unsuccessfull",
+    //     error: err,
+    //   });
+    // }
+    
     return res.send({
       status: 200,
       message: "Logout Sucessfully",
       data: user,
     });
   });
-});
+// });
 
 module.exports = AuthRouter;
